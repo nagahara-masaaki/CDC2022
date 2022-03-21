e7a9efb7d2ce43850e9ede1d20907f4b8d2ed4ed
Masaaki Nagahara <nagahara@ieee.org>
2022-03-21 17:43:47 +0900

    Add files via upload

--- Proj_LMI.m
+++ Proj_LMI.m
@@ -1,21 +1,30 @@
-function [Xopt,Yopt] = Proj_LMI_4(X0,Y0,X,Y,LMI1)
+function [Xopt,Yopt,info] = Proj_LMI_4(X0,Y0,X,Y,LMI1)
 
 ops=sdpsettings;
 ops=sdpsettings(ops,'verbose',0);
+%Fsolver='sdpt3';
+Fsolver='sedumi';
+%Fsolver='mosek';
 
 [mx,nx] = size(X0);
 [my,ny] = size(Y0);
 
-U = blkdiag(X,Y);
+U  = blkdiag(X,Y);
 U0 = blkdiag(X0,Y0);
 gamm = sdpvar(1,1);
-S = sdpvar(mx+my,mx+my);
+S11=sdpvar(mx,mx);
+S12=sdpvar(my,my);
+S1 =blkdiag(S11,S12);
+%S1= sdpvar(mx+my,mx+my);
+%S2= sdpvar(nx+ny,nx+ny);
 
-M3 = [S,U-U0; (U-U0)', eye(nx+ny)];
+M3 = [S1,U-U0; (U-U0)', eye(nx+ny)];
+%M3 = [S1,U-U0; (U-U0)', S2];
 
 LMI = [LMI1,M3>=0];
 
-optimize(LMI,trace(S),ops);
+info=optimize(LMI,trace(S1),ops);
+%info=optimize(LMI,trace(S1)+trace(S2),ops);
 
 Xopt = value(X);
 Yopt = value(Y);
